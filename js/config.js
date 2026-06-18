c5938e52fe86e00bc6480dc400791b89ee6a4f97
GGideonBonsai <ggideonstan@gmail.com>
2026-06-18 14:37:11 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,5 @@
 // config.js — Конфигурация Gideon Bonsai
 // Замените значения на ваши из Supabase → Settings → API
 
-export const SUPABASE_URL = 'https://oyimrrwsbleuhdnhfsub.supabase.co/rest/v1/';
+export const SUPABASE_URL = 'https://oyimrrwsbleuhdnhfsub.supabase.co';
 export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95aW1ycndzYmxldWhkbmhmc3ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MjQ3MzYsImV4cCI6MjA5NzIwMDczNn0.tJu6zTqDYFPZDhLh1tN2E0sfYxidRmzvdGxnfPwzkj8';
-
-// Название приложения
-export const APP_NAME = 'Gideon Bonsai';
